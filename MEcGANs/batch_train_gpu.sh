35fa768d27ee88e53380e618a8bdac763ec77be3
andrzejmizera <andrzej.mizera@gmail.com>
2021-12-11 14:11:05 +0100

    Delete MEcGANs directory

--- MEcGANs/batch_train_gpu.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -l
-#SBATCH -N 1
-#SBATCH --ntasks-per-node=4
-#SBATCH --time=2-00:00:00
-#SBATCH -p gpu
-#SBATCH --gpus=1
-
-CUDA_VISIBLE_DEVICES=0 ~/anaconda3/bin/python train_pix2pix.py --config_path configs/config_nirrgb2rgbcloud.yml --results_dir results
-
